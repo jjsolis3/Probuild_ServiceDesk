cb6e550cb5f3f824e018c94dc4f98ac6bb6d9d75
Jose J Solis <jjsolis3@asu.edu>
2026-03-23 12:58:53 -0700

    updated theme folder, and the app setting changes.

---
+++ Theme/Jobick/Jobick/Controllers/JobickController.cs
@@ -0,0 +1,337 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Jobick.Controllers
+{
+    public class JobickController : Controller
+    {
+		public IActionResult AddBlog()
+		{
+			return View();
+		}
+		public IActionResult AddEmail()
+		{
+			return View();
+		}
+		public IActionResult AppCalender()
+		{
+			return View();
+		}
+		public IActionResult ApplicationPage()
+		{
+			return View();
+		}
+		public IActionResult ApplyJob()
+		{
+			return View();
+		}
+		public IActionResult AppProfile()
+		{
+			return View();
+		}
+		public IActionResult Blog()
+		{
+			return View();
+		}
+		public IActionResult BlogCategory()
+		{
+			return View();
+		}
+		public IActionResult Chartist()
+		{
+			return View();
+		}
+		public IActionResult Chartjs()
+		{
+			return View();
+		}
+		public IActionResult Flot()
+		{
+			return View();
+		}
+		public IActionResult Morris()
+		{
+			return View();
+		}
+		public IActionResult Peity()
+		{
+			return View();
+		}
+		public IActionResult Sparkline()
+		{
+			return View();
+		}
+		public IActionResult Compaines()
+		{
+			return View();
+		}
+		public IActionResult Content()
+		{
+			return View();
+		}
+		public IActionResult ContentAdd()
+		{
+			return View();
+		}
+		public IActionResult Checkout()
+		{
+			return View();
+		}
+		public IActionResult Customers()
+		{
+			return View();
+		}
+		public IActionResult Invoice()
+		{
+			return View();
+		}
+		public IActionResult Detail()
+		{
+			return View();
+		}
+		public IActionResult ProductGrid()
+		{
+			return View();
+		}
+		public IActionResult List()
+		{
+			return View();
+		}
+		public IActionResult Order()
+		{
+			return View();
+		}
+		public IActionResult EditProfile()
+		{
+			return View();
+		}
+		public IActionResult Compose()
+		{
+			return View();
+		}
+		public IActionResult Inbox()
+		{
+			return View();
+		}
+		public IActionResult Read()
+		{
+			return View();
+		}
+		public IActionResult EmailTemplate()
+		{
+			return View();
+		}
+		public IActionResult EmptyPage()
+		{
+			return View();
+		}
+		public IActionResult Ckeditor()
+		{
+			return View();
+		}
+		public IActionResult Element()
+		{
+			return View();
+		}
+		public IActionResult Pickers()
+		{
+			return View();
+		}
+		public IActionResult Validation()
+		{
+			return View();
+		}
+		public IActionResult Wizard()
+		{
+			return View();
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Index2()
+		{
+			return View();
+		}
+		public IActionResult JobApplication()
+		{
+			return View();
+		}
+		public IActionResult JobList()
+		{
+			return View();
+		}
+		public IActionResult JobsPage()
+		{
+			return View();
+		}
+		public IActionResult JobView()
+		{
+			return View();
+		}
+		public IActionResult Jqvmap()
+		{
+			return View();
+		}
+		public IActionResult Menu()
+		{
+			return View();
+		}
+		public IActionResult MyProfile()
+		{
+			return View();
+		}
+		public IActionResult NewJob()
+		{
+			return View();
+		}
+		public IActionResult Error400()
+		{
+			return View();
+		}
+		public IActionResult Error403()
+		{
+			return View();
+		}
+		public IActionResult Error404()
+		{
+			return View();
+		}
+		public IActionResult Error500()
+		{
+			return View();
+		}
+		public IActionResult Error503()
+		{
+			return View();
+		}
+		public IActionResult ForgotPassword()
+		{
+			return View();
+		}
+		public IActionResult LockScreen()
+		{
+			return View();
+		}
+		public IActionResult Login()
+		{
+			return View();
+		}
+		public IActionResult Register()
+		{
+			return View();
+		}
+		public IActionResult PostDetails()
+		{
+			return View();
+		}
+		public IActionResult Statistics()
+		{
+			return View();
+		}
+		public IActionResult Bootstrap()
+		{
+			return View();
+		}
+		public IActionResult Datatable()
+		{
+			return View();
+		}
+		public IActionResult LightGallery()
+		{
+			return View();
+		}
+		public IActionResult Nestable()
+		{
+			return View();
+		}
+		public IActionResult Nouislider()
+		{
+			return View();
+		}
+		public IActionResult Select2()
+		{
+			return View();
+		}
+		public IActionResult Sweetalert()
+		{
+			return View();
+		}
+		public IActionResult Toastr()
+		{
+			return View();
+		}
+		public IActionResult Accordion()
+		{
+			return View();
+		}
+		public IActionResult Alert()
+		{
+			return View();
+		}
+		public IActionResult Badge()
+		{
+			return View();
+		}
+		public IActionResult Button()
+		{
+			return View();
+		}
+		public IActionResult ButtonGroup()
+		{
+			return View();
+		}
+		public IActionResult Card()
+		{
+			return View();
+		}
+		public IActionResult Carousel()
+		{
+			return View();
+		}
+		public IActionResult Dropdown()
+		{
+			return View();
+		}
+		public IActionResult Grid()
+		{
+			return View();
+		}
+		public IActionResult ListGroup()
+		{
+			return View();
+		}
+		public IActionResult Modal()
+		{
+			return View();
+		}
+		public IActionResult Pagination()
+		{
+			return View();
+		}
+		public IActionResult Popover()
+		{
+			return View();
+		}
+		public IActionResult ProgressBar()
+		{
+			return View();
+		}
+		public IActionResult Tab()
+		{
+			return View();
+		}
+		public IActionResult Typography()
+		{
+			return View();
+		}
+		public IActionResult UserProfile()
+		{
+			return View();
+		}
+		public IActionResult Widget()
+		{
+			return View();
+		}
+	}
+}
